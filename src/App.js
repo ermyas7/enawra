b91b87cbf8cb527a172a11ce3401f30cc60f5337
ermyas7 <ermyas.fekadu@gmail.com>
2019-11-06 22:38:07 +0530

    configure react router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React, {Fragment} from 'react';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+
 import './assets/sass/main.scss';
 import Header from './layouts/header';
 import Home from './pages/home';
+import About from './pages/about';
+
 function App() {
   return (
-    <Fragment>
+    <Router>
+      <Fragment>
       <Header/>
-      <Home/>
+      <Switch>
+        <Route exact path="/" component={Home}/>
+        <Route exact path="/about" component={About}/>
+      </Switch>
     </Fragment>
+    </Router>
   );
 }
 
