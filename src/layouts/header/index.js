48514f538a76c204ff3488308b4e71f89d0cf5e9
ermyas7 <ermyas.fekadu@gmail.com>
2019-11-06 23:23:04 +0530

    Add header

--- src/layouts/header/index.js
+++ src/layouts/header/index.js
@@ -1,10 +1,32 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 import './header.scss';
 const index = () => {
     return (
-        <div>
-          <h1>Header</h1>  
-        </div>
+        <header className="header">
+            <div className="navigation">
+                <ul className="navigation-list">
+                    <li className="navigation-item">
+                    <i class="fas fa-bars"></i>
+                    </li>
+                    <li className="navigation-item">
+                        <Link className="navigation-link" to="/">Home</Link>
+                    </li>
+                    <li className="navigation-item">
+                        <Link className="navigation-link" to="#">Home</Link>
+                    </li>
+                    <li className="navigation-item">
+                        <Link className="navigation-link" to="#">Home</Link>
+                    </li>
+                    <li className="navigation-item">
+                        <Link className="navigation-link" to="#">Home</Link>
+                    </li>
+                    <li className="navigation-item">
+                        <Link className="navigation-link" to="#">Home</Link>
+                    </li>
+                </ul>
+            </div>
+        </header>
     )
 }
 
