1b784d2a38fbeb3d94a13ad6c3ac760aa633fc48
ermyas7 <ermyas.fekadu@gmail.com>
2019-11-07 16:58:14 +0530

    Add add post functionality

--- src/components/blogForm/AddBlog.js
+++ src/components/blogForm/AddBlog.js
@@ -1,20 +1,25 @@
 import React from 'react'
 import './blogForm.scss'
-const AddBlog = (props) => {
+const AddBlog = ({post, changeHandler, addPost}) => {
     return (
         <div className="add-blog">
             <input type="text" className="add-blog-title"
             name="title"
             placeholder="title"
-            value={props.title}/>
-            <textarea name="body" id="body" cols="10" rows="4" className="add-blog-body">{props.title}</textarea>
+            value={post.title}
+            onChange={(evt) => changeHandler(evt)}/>
+            <textarea name="body" id="body" cols="10" rows="4" className="add-blog-body"
+            onChange={(evt) => changeHandler(evt)}>{post.title}</textarea>
             <input type="text" className="add-blog-img"
             name="image"
-            placeholder="image" value={props.image}/>
+            placeholder="image" value={post.image}
+            onChange={(evt) => changeHandler(evt)}/>
             <input type="text" className="add-blog-tages"
-            name="tages"
-            placeholder="tages" value={props.tags}/>
-            <button className="add-blog-submit">
+            name="tags"
+            placeholder="tags" value={post.tags}
+            onChange={(evt) => changeHandler(evt)}/>
+            <button className="add-blog-submit"
+            onClick={addPost}>
                 Submit
             </button>
         </div>
--- src/pages/blogs/index.js
+++ src/pages/blogs/index.js
@@ -1,11 +1,12 @@
 import React, {useState} from 'react'
+import uuidv1 from 'uuid/v1';
 import './blogs.scss'
 import BlogItem from './BlogItem'
 import AddBlog from '../../components/blogForm/AddBlog';
 
 const mockPosts = [
     {
-        id: 1,
+        id: '1',
         title: 'Introduction',
         body: 't. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using Content here, content here, making it look like readable English.',
         author: 'Ermyas',
@@ -14,7 +15,7 @@ const mockPosts = [
         date: 'oct 22, 2019'
     },
     {
-        id: 2,
+        id: '2',
         title: 'Greetings',
         body: ' The point of using Lorem Ipsum is that it has a more-or-less normal',
         author: 'Ermyas',
@@ -23,7 +24,7 @@ const mockPosts = [
         date: 'oct 15, 2019'
     },
     {
-        id: 3,
+        id: '3',
         title: 'Action',
         body: ' The point of using Lorem Ipsum is that it has a more-or-less normal',
         author: 'Bereket',
@@ -37,13 +38,37 @@ const mockPosts = [
  const Index = () => {
      const [posts, setPosts] = useState(mockPosts);
      const [addPost, setAddPost] = useState(false);
+    const [post, setPost] = useState({});
 
+    const _changeHandler = (evt) => {
+        const {name, value} = evt.target;
+        post[name] = value;
+        setPost(post);
+    }
+
+    const _addPost = () => {
+        console.log(post);
+        if(post.title && post.body&&post.image && post.tags){
+            post.id = uuidv1();
+            post.author = 'Muna';
+            post.date = 'oct 15, 2019';
+            post.tags = [...post.tags.split(" ")];
+            const newPosts = [...posts,post];
+            console.log(newPosts);
+            setPosts(newPosts);
+            setPost({});
+            setAddPost(false);
+        }
+    }
     return (
         <div className="blog">
-            {/* {   !addPost?
+            {  
+                !addPost?
                 <button className="blog-add"
-                onClick={() => setAddPost(true)}>Add Blog</button> : <AddBlog/>  */
-                <AddBlog/>}
+                onClick={() => setAddPost(true)}>Add Blog</button> :
+                <AddBlog changeHandler = {_changeHandler} post={post}
+                addPost={_addPost}/>
+                }
 
             <div className="blog-lists">
                 {
