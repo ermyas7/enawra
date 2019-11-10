09a96c9c545299e2bb29d6c173498253616148b7
ermyas7 <ermyas.fekadu@gmail.com>
2019-11-10 13:03:46 +0530

    Post from firebase

--- src/pages/blogs/index.js
+++ src/pages/blogs/index.js
@@ -1,44 +1,14 @@
 import React, {useState, useEffect} from 'react'
-import uuidv1 from 'uuid/v1'
 
 import {firestore} from '../../firebase'
+import {collectIdsAndDocs} from '../../utilis'
+
 import './blogs.scss'
 import BlogItem from './BlogItem'
-import AddBlog from '../../components/blogForm/AddBlog';
-
-const mockPosts = [
-    {
-        id: '1',
-        title: 'Introduction',
-        body: 't. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using Content here, content here, making it look like readable English.',
-        author: 'Ermyas',
-        tags: ['sport', 'life style', 'fun'],
-        image: 'https://source.unsplash.com/user/erondu/1600x900',
-        date: 'oct 22, 2019'
-    },
-    {
-        id: '2',
-        title: 'Greetings',
-        body: ' The point of using Lorem Ipsum is that it has a more-or-less normal',
-        author: 'Ermyas',
-        tags: ['sport', 'life style', 'fun'],
-        image: 'https://source.unsplash.com/user/eron/1600x900',
-        date: 'oct 15, 2019'
-    },
-    {
-        id: '3',
-        title: 'Action',
-        body: ' The point of using Lorem Ipsum is that it has a more-or-less normal',
-        author: 'Bereket',
-        tags: ['sport', 'coffee', 'faffy'],
-        image: 'https://source.unsplash.com/user/ero/1600x900',
-        date: 'oct 15, 2019'
-    },
-
-]
+import AddBlog from '../../components/blogForm/AddBlog'
 
  const Index = () => {
-     const [posts, setPosts] = useState(mockPosts);
+     const [posts, setPosts] = useState([]);
      const [addPost, setAddPost] = useState(false);
     const [post, setPost] = useState({});
 
@@ -51,7 +21,6 @@ const mockPosts = [
     const _addPost = () => {
         console.log(post);
         if(post.title && post.body&&post.image && post.tags){
-            post.id = uuidv1();
             post.author = 'Muna';
             post.date = 'oct 15, 2019';
             post.tags = [...post.tags.split(",")];
@@ -63,11 +32,16 @@ const mockPosts = [
         }
     }
 
-    useEffect(() => {
-        firestore.collection('posts')
+    const getPosts = async () => {
+        const snapshot = await firestore.collection('posts')
         .get()
-        .then(snap => console.log({snap}))
-    })
+        const posts = snapshot.docs.map(collectIdsAndDocs)
+        console.log(posts)
+        setPosts(posts)
+    }
+    useEffect( () => {
+      getPosts()  
+    }, [])
     return (
         <div className="blog">
             {  
